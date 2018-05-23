608b22b31a20ee55498fbba36e1c074cfd8aedb7
Cory <cbosselmann1029@gmail.com>
2018-05-23 18:13:37 -0500

    got all components set up

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -8,23 +8,6 @@ function TopNav() {
           <ul className="dropdown-menu message-dropdown">
 
               {/*  <MessagePreview>   */}
-              <li className="message-preview">
-                  <a href="#">
-                      <div className="media">
-                          <span className="pull-left">
-                              <img className="media-object" src="http://placehold.it/50x50" alt="" />
-                          </span>
-                          <div className="media-body">
-                              <h5 className="media-heading"><strong>John Smith</strong>
-                              </h5>
-                              {/*  <DateTime>   */}
-                              <p className="small text-muted"><i className="fa fa-clock-o"></i> Yesterday at 4:32 PM</p>
-                              {/*  </DateTime>   */}
-                              <p>Lorem ipsum dolor sit amet, consectetur...</p>
-                          </div>
-                      </div>
-                  </a>
-              </li>
               {/*  </MessagePreview>   */}
 
 
